d6a1ce29ffa507162c4766e0990762bf2ac7b7f5
Seth Winslow <swwinslow@gmail.com>
2017-06-27 10:18:47 -0400

    Big Update

--- routes/results.js
+++ routes/results.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var cookieParser = require('cookie-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-  res.cookie('cookieName', 'cookieValue')
-   res.render('game4', { title: 'Express' });
-
-
+  res.render('results', {title: 'example'});
 });
 
 module.exports = router;
